6cc24280d94a6736c4c249751beccf1933debc8d
soumia elyakni <selyakni@gmail.com>
2024-08-24 03:17:20 +0100

    Add link to BCard

--- norophilia-cli/src/components/ComingSoon.jsx
+++ norophilia-cli/src/components/ComingSoon.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import backgroundImage from "../pictures/Background.jpeg";
 import { useTranslation } from "react-i18next";
 import "../i18n";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 import logo from "../pictures/LogoNph.png";
 
 const ComingSoon = () => {
@@ -35,23 +37,21 @@ const ComingSoon = () => {
         </select>
       </div>
 
-      <div className="absolute inset-0 bg-black bg-opacity-30 backdrop-blur-sm"></div>
+      <div className="filtre absolute inset-0 bg-black bg-opacity-30 backdrop-blur-sm"></div>
 
-      {/* Contenu de la page */}
-      <div className="relative flex-col z-10 text-center bg-white bg-opacity-60 p-8 w-full lg:w-8/12 rounded-lg shadow-lg">
+      <div className="page-content relative flex-col z-10 text-center bg-white bg-opacity-60 p-8 w-full lg:w-8/12 rounded-lg shadow-lg">
         <header className="top-0 left-0 right-0 p-4 flex justify-center">
-          {/* <div className="bg-opacity-10 p-4 rounded-md shadow-s"> */}
-          <img src={logo} alt="Logo" className="h-logo" />{" "}
-          {/* Increase logo size */}
-          {/* </div> */}
+          <img src={logo} alt="Logo" className="h-logo" />
         </header>
         <h1 className="text-2xl md:text-2xl font-bold text-navy mb-6 text-blue-950">
           {t("heading")}
         </h1>
+        <div className="form-label mb-2 text-blue-950 ">{t("label")}</div>
         <form
           onSubmit={handleSubmit}
           className="flex justify-center w-full items-center"
         >
+            
           <div className="relative w-full max-w-xs mb-4">
             <input
               type="email"
@@ -67,25 +67,15 @@ const ComingSoon = () => {
           </div>
         </form>
 
-        <div className="flex justify-center space-x-4 my-4">
-          {/* Icônes de réseaux sociaux */}
+        <div className="flex justify-center space-x-4 my-4 ">
+          {/* Lien vers la carte de visite */}
           <a
-            href="https://facebook.com"
+            href="https://norophilia-bc.vercel.app/" // Remplace par le lien vers ta carte de visite
+            target="_blank" // Opens in a new tab
+            rel="noopener noreferrer" // Adds security
             className="text-gray-600 hover:text-gray-900"
           >
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a
-            href="https://twitter.com"
-            className="text-gray-600 hover:text-gray-900"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a
-            href="https://instagram.com"
-            className="text-gray-600 hover:text-gray-900"
-          >
-            <i className="fab fa-instagram"></i>
+            <FontAwesomeIcon icon={faAddressCard} size="2x" /> {/* Remplace par une icône pertinente si nécessaire */}
           </a>
         </div>
       </div>
