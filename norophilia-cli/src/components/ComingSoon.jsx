36436f8a73530dc6aad5c46b546c7a885dfb6aa2
soumia elyakni <selyakni@gmail.com>
2024-08-25 00:41:22 +0100

    Change BusinessCard link icone

--- norophilia-cli/src/components/ComingSoon.jsx
+++ norophilia-cli/src/components/ComingSoon.jsx
@@ -3,7 +3,7 @@ import backgroundImage from "../pictures/Background.jpeg";
 import { useTranslation } from "react-i18next";
 import "../i18n";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import {faHandshake} from '@fortawesome/free-solid-svg-icons';
 import logo from "../pictures/LogoNph.png";
 
 const ComingSoon = () => {
@@ -39,7 +39,7 @@ const ComingSoon = () => {
 
       <div className="filtre absolute inset-0 bg-black bg-opacity-30 backdrop-blur-sm"></div>
 
-      <div className="page-content relative flex-col z-10 text-center bg-white bg-opacity-60 p-8 w-full lg:w-8/12 rounded-lg shadow-lg">
+      <div className="page-content mb-7 flex-col z-10 text-center bg-white bg-opacity-60 p-7 w-full lg:w-8/12 rounded-lg shadow-lg">
         <header className="top-0 left-0 right-0 p-4 flex justify-center">
           <img src={logo} alt="Logo" className="h-logo" />
         </header>
@@ -75,7 +75,7 @@ const ComingSoon = () => {
             rel="noopener noreferrer" // Adds security
             className="text-gray-600 hover:text-gray-900"
           >
-            <FontAwesomeIcon icon={faAddressCard} size="2x" /> {/* Remplace par une icône pertinente si nécessaire */}
+            <FontAwesomeIcon icon={faHandshake} size="2x" /> {/* Remplace par une icône pertinente si nécessaire */}
           </a>
         </div>
       </div>
